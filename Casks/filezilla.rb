ad3e25662befc00aa84df0294a9cb78a80f87524
w <>
2025-04-17 18:27:40 +0800

    fix

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -2,7 +2,7 @@ cask "filezilla" do
     version "3.69.0.1"
     sha256 "ef0d3ae8e89d3ce82b9e1478d51335e995bbd8c1b6d801744178753f59dcc6c4" # 替换实际校验和
   
-    url "https://dl2.cdn.filezilla-project.org/client/FileZilla_3.69.0.1_macos-x86.app.tar.bz2?h=ImooXIDJX4jBql06Exrw4A&x=1744888223https://dl2.cdn.filezilla-project.org/client/FileZilla_3.68.1_macos-x86.app.tar.bz2?h=ddVq72GHiHbT8rnK_mA-fg&x=1743756592"
+    url "https://dl2.cdn.filezilla-project.org/client/FileZilla_3.69.0.1_macos-x86.app.tar.bz2?h=ImooXIDJX4jBql06Exrw4A&x=1744888223"
     name "FileZilla"
     desc "Cross-platform FTP client with GUI"
     homepage "https://filezilla-project.org/"
@@ -14,4 +14,3 @@ cask "filezilla" do
       "~/Library/Preferences/org.filezilla-project.filezilla.plist",
     ]
   end
-  
