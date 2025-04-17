1f2703c39946ca3a9de5903a655bf65206301f7a
w <>
2025-04-17 17:26:07 +0800

    sha256

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -1,6 +1,6 @@
 cask "filezilla" do
     version "3.68.1"
-    sha256 "0ceeffd68816d46e905c286327592ee7999d3569842675526c1e95cbd7209bc9" # 替换实际校验和
+    sha256 "ef0d3ae8e89d3ce82b9e1478d51335e995bbd8c1b6d801744178753f59dcc6c4" # 替换实际校验和
   
     url "https://dl2.cdn.filezilla-project.org/client/FileZilla_3.68.1_macos-x86.app.tar.bz2?h=ddVq72GHiHbT8rnK_mA-fg&x=1743756592"
     name "FileZilla"
