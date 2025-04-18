ba881326f41dae02a2ad59a81027ef05216fc473
w <>
2025-04-18 15:03:33 +0800

    豆包

---
+++ Casks/doubao.rb
@@ -0,0 +1,12 @@
+# Casks/app1.rb
+cask "doubao" do
+  version "1.52.8"
+  sha256 "cc88d1522d0dea3150d3f7e880e62ece9e21ace2a142fb945f7236d22e809299"
+
+  url "https://lf-flow-web-cdn.doubao.com/obj/flow-doubao/doubao_pc/1.52.8/1005/Doubao_universal.dmg"
+  name "doubao"
+  desc "豆包"
+  homepage "https://quark.cn/"
+
+  app "doubao.app"
+end
